6df601ac6760b910a6946048ba3cc1301353d0e3
Jhonatha1 <yankyboa@gmail.com>
2023-10-04 20:09:57 -0300

    Configs de deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import{Outlet} from 'react-router-dom'
 import Navbar from './components/Navbar.jsx'
-
+import { Helmet } from 'react-helmet';
 
 import './App.css'
 
@@ -9,6 +9,7 @@ function App() {
 
   return (
       <div className="App">
+        
        <Navbar/>
 
       <Outlet/>
