714db53d26495e7482bf4f62f655eb9215bb2a17
Jhonatha1 <yankyboa@gmail.com>
2023-10-04 20:26:49 -0300

    Configs de deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {HashRouter, Routes, Route} from 'react-router-dom'
 
 
 
@@ -15,7 +15,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-<BrowserRouter> 
+<HashRouter> 
 <Routes>
   <Route element={<App/>}>
   <Route path="/"element= {<Home/>}/>
@@ -25,6 +25,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </Route>
 </Routes>
 
- </BrowserRouter>
+ </HashRouter>
   </React.StrictMode>,
 )
